fbbdf9fc5fff0a0ad1a54cbfb810097d000baae8
socratone <gim2origin@gmail.com>
2020-07-31 02:08:40 +0900

    exponentialRampToValueAtTime을 이용한 fadeout 효과

--- src/App.js
+++ src/App.js
@@ -1,64 +1,54 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
-let AudioContext;
+const AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioContext;
 let gainNode;
 
-const MASTER_VOLUME = 1;
-
 function App() {
-  const audio = React.createRef();
+  const audioFile = React.createRef();
+
+  useEffect(() => {
+    audioContext = new AudioContext();
+
+    // pass it into the audio context
+    const source = audioContext.createMediaElementSource(audioFile.current);
+
+    // 게인 조절
+    gainNode = audioContext.createGain();
+
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+    source.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+  }, []);
 
   const clickPlayButton = () => {
     // check if context is in suspended state (autoplay policy)
     if (audioContext.state === 'suspended') {
       audioContext.resume();
     }
-    gainNode.gain.value = MASTER_VOLUME;
-    audio.current.play();
+    gainNode.gain.setValueAtTime(1, audioContext.currentTime);
+    audioFile.current.play();
   };
 
   const clickPauseButton = () => {
     if (audioContext.state === 'suspended') {
       audioContext.resume();
     }
-    audio.current.pause();
+    audioFile.current.pause();
   };
 
   const clickFadeButton = () => {
-    const INTERVAL = 1;
-    let volume = MASTER_VOLUME;
-
-    const fadeout = setInterval(() => {
-      volume -= 0.02;
-      if (volume > -1) {
-        gainNode.gain.value = volume.toFixed(2);
-      } else {
-        clearInterval(fadeout);
-        gainNode.gain.value = -1;
-        audio.current.pause();
-        audio.current.currentTime = 0;
-      }
-    }, INTERVAL);
+    gainNode.gain.setValueAtTime(gainNode.gain.value, audioContext.currentTime);
+    gainNode.gain.exponentialRampToValueAtTime(
+      0.001,
+      audioContext.currentTime + 1
+    );
   };
-
-  useEffect(() => {
-    AudioContext = window.AudioContext || window.webkitAudioContext;
-    audioContext = new AudioContext();
-
-    // pass it into the audio context
-    const track = audioContext.createMediaElementSource(audio.current);
-    track.connect(audioContext.destination);
-
-    // 게인 조절
-    gainNode = audioContext.createGain();
-    track.connect(gainNode).connect(audioContext.destination);
-  }, []);
 
   return (
     <div className="App">
-      <audio src="e4.mp3" ref={audio}></audio>
+      <audio src="e4.mp3" ref={audioFile}></audio>
       <button onClick={clickPlayButton}>play</button>
       <button onClick={clickPauseButton}>pause</button>
       <button onClick={clickFadeButton}>fade</button>
